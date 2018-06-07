913905e1694180daf23607636487c3626c4c9ea7
Wal McConnell <walter.mcconnell@gmail.com>
2018-06-07 18:20:06 +0100

    simple rolling average & EWMA

--- app.R
+++ app.R
@@ -1,16 +1,20 @@
 library(tidyverse)
 library(lubridate)
 library(shiny)
+library(tidyquant)
+library(purrr)
 library(zoo)
+library(qcc)
 
 DEFAULT_ACUTE_PERIOD = 7
 DEFAULT_CHRONIC_PERIOD = 28
+ACR_METHODS = c("Simple Rolling Average" = "simple_ra", 
+                "EWMA" = "ewma")
 
 dataset <- read_csv('data/adam_sullivan_load_data.csv')
 dataset$date <- dmy(dataset$date)
 dataset$week <- week(dataset$date)
 athletes <- unique(dataset$athlete)
-
 
 unique(dataset$date)
 
@@ -24,14 +28,17 @@ ui <- fluidPage(
    sidebarLayout(
       sidebarPanel(
         selectInput("athlete", h4("Athlete"), choices = athletes, selected = head(athletes, 1)),
-        p("Note: Acute Period is fixed at 7 Days"),
-        sliderInput("chronic_period", h4("Chronic Period (days)"), min = 7, max = 28, step = 7, value = DEFAULT_CHRONIC_PERIOD)
-      ),
-      
+        sliderInput("ac_periods", 
+                    h4("Acute - Chronic Periods (days)"), 
+                    min = 7, 
+                    max = 42, 
+                    step = 7, 
+                    value = c(DEFAULT_ACUTE_PERIOD, DEFAULT_CHRONIC_PERIOD)),
+        checkboxGroupInput("methods", h4("Analysis Method"), ACR_METHODS, selected=ACR_METHODS[1])
+        ),
       mainPanel(
         plotOutput("acrPlot"),
-         plotOutput("acute_chronicPlot"),
-        plotOutput("rawDataPlot")
+        dataTableOutput("acrData")
       )
    )
 )
@@ -39,40 +46,70 @@ ui <- fluidPage(
 server <- function(input, output) {
 
   acute_period <- reactive({
-    DEFAULT_ACUTE_PERIOD
+    input$ac_periods[1]
   })
   
   chronic_period <- reactive({
-    input$chronic_period
+    input$ac_periods[2]
   })
   
-  filtered_dataset <- reactive({
-    data <- dataset %>% filter(athlete == input$athlete)
+  filtered_data <- reactive({
+    dataset %>% filter(athlete == input$athlete)
+  })
+  
+  simple_rolling_average_AC <- function(data, acute_period, chronic_period) {
     data %>% 
-      mutate(acute = rollapply(load, acute_period(), FUN = mean, na.rm = TRUE, fill = NA, align = "right"),
-             chronic = rollapply(load, chronic_period(), FUN = mean, na.rm = TRUE, fill = NA, align = "right"),
-             acr = acute / chronic) 
+      mutate(
+        method = "simple_ra",
+        acute = rollapply(load, acute_period, FUN = mean, na.rm = TRUE, fill = NA, align = "right"),
+        chronic = rollapply(load, chronic_period, FUN = mean, na.rm = TRUE, fill = NA, align = "right"),
+        acr = acute / chronic) %>%
+      gather(statistic, value, acute, chronic, acr)
+  }
+  
+  ewma_AC <- function(data, acute_period, chronic_period) {
+    #mutate(Chronic_weighted = Player.Load *0.07 + (1-0.07) * lag(Chronic),
+    #       Acute_weighted = Player.Load * 0.25 + (1-0.25)* lag(Acute))
+    
+    acute_alpha <- 0.25 #2/(acute_period+1)
+    chronic_alpha <- 0.09 #2/(chronic_period+1)
+    
+    data %>%
+      mutate(
+        method = "ewma",
+        acute = load * acute_alpha + (1-acute_alpha) * lag(load),
+        chronic = load * chronic_alpha + (1-chronic_alpha) * lag(load),
+        acr = acute / chronic) %>%
+      gather(statistic, value, acute, chronic, acr)
+  }
+  
+  perform_analysis <- function(x) {
+    analysis_type <- x
+    data <- filtered_data()
+    acute_period <- acute_period()
+    chronic_period <- chronic_period()
+    
+    if(analysis_type == "simple_ra") {
+      simple_rolling_average_AC(data, acute_period, chronic_period)
+    } else {
+      ewma_AC(data, acute_period, chronic_period)
+    } 
+  }
+  
+  analysed_data <- reactive({
+    print(input$methods)
+    input$methods %>% map(perform_analysis) %>% reduce(rbind)
   })
 
-   output$rawDataPlot <- renderPlot({
-     
-      ggplot(filtered_dataset(), aes(date, load, fill = factor(week %% 2))) + 
-        geom_bar(stat = "identity") +
-        scale_color_discrete() +
-        theme_minimal()
-   })
-   
-   output$acute_chronicPlot <- renderPlot({
-     ggplot(filtered_dataset(), aes(date)) + 
-       geom_line(aes(y = acute), color = "blue") + 
-       geom_line(aes(y = chronic), color = "red") + 
-       theme_minimal()
-   })
-   
    output$acrPlot <- renderPlot({
-     ggplot(filtered_dataset(), aes(date, acr)) + 
+     data <- analysed_data() %>% filter(statistic == 'acr')
+     ggplot(data, aes(date, value, color = method)) + 
        geom_line() + 
        theme_minimal()
+   })
+   
+   output$acrData <- renderDataTable({
+     analysed_data() %>% spread(statistic, value)
    })
 }
 
